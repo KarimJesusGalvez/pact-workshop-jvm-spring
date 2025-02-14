5b9077661034db865a4098c95e254cdb3c71759d
KJAMRAOUI <KarimJesus.AmraouiGalvez@ust.com>
2025-02-14 08:38:43 +0100

    Added logging dependency

--- consumer/src/main/java/au/com/dius/pactworkshop/consumer/ConsumerApplication.java
+++ consumer/src/main/java/au/com/dius/pactworkshop/consumer/ConsumerApplication.java
@@ -2,11 +2,13 @@ package au.com.dius.pactworkshop.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class ConsumerApplication {
 
 	public static void main(String[] args) {
+		LoggerFactory.getLogger(ConsumerApplication.class).info("Consumer Started");
 		SpringApplication.run(ConsumerApplication.class, args);
 	}
 
